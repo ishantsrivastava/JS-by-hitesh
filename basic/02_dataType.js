c5bee18605012afd530112550dff0b95fd967501
Ishant Srivastava <65581603+ishantsrivastava@users.noreply.github.com>
2024-05-16 09:03:10 +0000

    datatype

---
+++ basic/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3+3) we are using node js , not browser
+
+// console.log(3+3)
+// console.log(typeof "ishant")
+
+let name = "ishant"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 5
+// bigInt
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>undefined
+// symbol => unique
+
+
+
+console.log(typeof(state))
+console.log(typeof age)
+console.log(typeof(isLoggedIn))
+console.log(typeof null)
